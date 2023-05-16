257f04372a6252c3e78ebc76f2024bf3d1e36ab3
apsei <112944138+apsei@users.noreply.github.com>
2023-05-16 17:10:20 +0300

    new page (about)

--- q-service/js/modals.js
+++ q-service/js/modals.js
@@ -1,40 +1,34 @@
-// getting service cards
 var cards = document.querySelectorAll(".service-card");
 
-// getting modals
 var modals = document.querySelectorAll(".modal");
 
-// getting closing spans
 var spans = document.querySelectorAll(".close");
 
-// modal show when click on window
 cards.forEach((card) => {
-    card.addEventListener('click', function() {
-        var category = this.getAttribute('data-category');
-        modals.forEach((modal) => {
-            if(modal.getAttribute('data-modal') === category) {
-                modal.style.display = "block";
-            }
-        });
-    });
-});
-
-// window close when x clicked
-spans.forEach((span) => {
-    span.addEventListener('click', function() {
-        modals.forEach((modal) => {
-            if(window.getComputedStyle(modal).display === "block") {
-                modal.style.display = "none";
-            }
-        });
-    });
-});
-
-// window close if click somewhere besides
-window.onclick = function(event) {
+  card.addEventListener('click', function () {
+    var category = this.getAttribute('data-category');
     modals.forEach((modal) => {
-        if(event.target == modal) {
-            modal.style.display = "none";
-        }
+      if (modal.getAttribute('data-modal') === category) {
+        modal.style.display = "block";
+      }
     });
+  });
+});
+
+spans.forEach((span) => {
+  span.addEventListener('click', function () {
+    modals.forEach((modal) => {
+      if (window.getComputedStyle(modal).display === "block") {
+        modal.style.display = "none";
+      }
+    });
+  });
+});
+
+window.onclick = function (event) {
+  modals.forEach((modal) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
 }
